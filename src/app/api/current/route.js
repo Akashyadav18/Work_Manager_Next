b2590360af41f2b97dbbc8f4e4fbe4519fe0f2ad
Akashyadav18 <shadowlee5469@gmail.com>
2023-12-01 20:33:44 +0530

    changes

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -2,17 +2,18 @@ import { User } from "@/models/user";
 import jwt from "jsonwebtoken"
 import { NextResponse } from "next/server";
 
-export async function GET(request){
+export async function getServerSideProps(context) {
     try {
-        const token = request.cookies.get("token") ?.value;
+        const token = context.req.cookies.token;
         if (!token) {
-            throw new Error('No token provided');
+            return { props: {} };
         }
         const data = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
         const user = await User.findById(data.id).select("-password")
-        return NextResponse.json(user)
+        return { props: { user } };
     } catch (error) {
         console.log(error);
-        throw new Error(error.message);
+        return { props: {} };
     }
 }
+
